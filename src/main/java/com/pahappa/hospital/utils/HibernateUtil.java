c39faaad04aadb813641c0eef9e66046cfb56919
Peter Bukama Mulinzi <pbmulinzi@gmail.com>
2025-07-18 14:59:19 +0300

    Changes in the hibernate util file
    1. Made the class a managed bean
    2. Added "produces" to the getSessionFactory method to produce it for injection.

--- src/main/java/com/pahappa/hospital/utils/HibernateUtil.java
+++ src/main/java/com/pahappa/hospital/utils/HibernateUtil.java
@@ -1,15 +1,30 @@
 package com.pahappa.hospital.utils;
 
+
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+
+@ApplicationScoped  //making it a CDI managed bean
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory = buildSessionFactory();
 
-    private static SessionFactory buildSessionFactory() {
+    private static final Logger logger = LoggerFactory.getLogger(HibernateUtil.class);
+
+    private final SessionFactory sessionFactory;
+
+    public HibernateUtil() {  //constructor initialization
+        this.sessionFactory = buildSessionFactory();
+    }
+
+    private SessionFactory buildSessionFactory() {
         try {
             // Create registry using hibernate.cfg.xml
             StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -17,10 +32,9 @@ public class HibernateUtil {
                     .build();
 
             // Create MetadataSources
-            MetadataSources sources = new MetadataSources(registry);
-
-            // Create Metadata
-            Metadata metadata = sources.getMetadataBuilder().build();
+            Metadata metadata = new MetadataSources(registry)
+                    .getMetadataBuilder()
+                    .build();
 
             // Create SessionFactory
             return metadata.getSessionFactoryBuilder().build();
@@ -31,12 +45,16 @@ public class HibernateUtil {
         }
     }
 
-    public static SessionFactory getSessionFactory() {
+    @Produces //to produce the SessionFactory for injection
+    public SessionFactory getSessionFactory() {
         return sessionFactory;
     }
 
-    public static void shutdown() {
-        if (sessionFactory != null) {
+
+    @PreDestroy  // Called when application shuts down
+    public void shutdown() {
+        if (sessionFactory != null && !sessionFactory.isClosed()) {
+            logger.info("Closing Hibernate SessionFactory");
             sessionFactory.close();
         }
     }
